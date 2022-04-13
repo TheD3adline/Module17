679dbd51bafa7fbcab5a222e7b6d199ba206a8a9
MichaelFessler-Desktop <michaelfessler@gmx.at>
2022-04-13 08:40:56 +0200

    Forward unto Dawn!

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,16 +2,20 @@ package com.company;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception { //This throws Exception extension is required for the most basic form of exception, it "catches" the exception thrown from Person person1, delivers a message to console and stops the rest of the code.
+                                                                //It is NOT needed if the exception is handled with a try/catch further down. There is nothing that needs to be passed further up.
+        //Exceptions are customized deviations or special cases. At their most basic they kinda work like a constructor would with inheritance from higher classes and the super command.
+        //They can be checked with for example an if statement within a method, in the basic setting stop the rest of the code from being executed and hand a customized or not message "up" into the class that executes the call.
 
         Person person1 = new Person(90);
 
-        try {
+        try { //This is a try/catch block, it can execute a method, and if the executed method throws a standard or custom exception, it catches it and executes the code within the catch(){} part, and then resumes with executing the code below the try/catch block.
             person1.sprint();
         } catch(Exception e) {
             System.out.println("The person is too old to sprint...");
         }
 
         System.out.println("test");
+
     }
 }
--- src/com/company/Person.java
+++ src/com/company/Person.java
@@ -8,9 +8,9 @@ public class Person {
         this.age = age;
     }
 
-    public void sprint() throws Exception {
-        if(this.age > 65) {
-            throw new Exception();
+    public void sprint() throws Exception { //this is the extension of the method required to throw the standard or customized exception
+        if(this.age > 65) { //condition to check if the exception applies, if it applies it throws the standard or customized exception and stops the execution of the rest of the code
+            throw new Exception(); //most basic form of exception, the standard that is part of the basic Java toolset, stops rest of code if triggered
         }
         System.out.println("The person sprints...");
     }
