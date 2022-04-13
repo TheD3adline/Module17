8516111e4ab3f6062da6e6880fdeaae3ae0bb315
MichaelFessler-Desktop <michaelfessler@gmx.at>
2022-04-13 10:05:28 +0200

    Forward unto Dawn!

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,14 +7,14 @@ public class Main {
         //Exceptions are customized deviations or special cases. At their most basic they kinda work like a constructor would with inheritance from higher classes and the super command.
         //They can be checked with for example an if statement within a method, in the basic setting stop the rest of the code from being executed and hand a customized or not message "up" into the class that executes the call.
 
-        Person person1 = new Person(90);
+        Person person1 = new Person(20);
 
         try { //This is a try/catch block, it can execute a method, and if the executed method throws a standard or custom exception, it catches it and executes the code within the catch(){} part, and then resumes with executing the code below the try/catch block.
             person1.sprint();
         } catch(Exception e) {
-            System.out.println("The person is too old to sprint...");
-        }
-
+            System.out.println("The person is too old to sprint..."); //Since we deliberately introduced an arithmetic exception in sprint() method of Person.java, and set this method up to throw Exception, and this catch to catch it, we will still trigger
+        }                                                             //this line even though we changed the age of person1 during initialization to not trigger the age check in Person.sprint() method anymore. Usually an arithmetic exception is a RuntimeException
+                                                                      //that would terminate the program, but since we defined how to handle it here, the rest of the code will still be executed.
         System.out.println("test");
 
     }
--- src/com/company/Person.java
+++ src/com/company/Person.java
@@ -12,6 +12,7 @@ public class Person {
         if(this.age > 65) { //condition to check if the exception applies, if it applies it throws the standard or customized exception and stops the execution of the rest of the code
             throw new Exception(); //most basic form of exception, the standard that is part of the basic Java toolset, stops rest of code if triggered
         }
-        System.out.println("The person sprints...");
-    }
+        System.out.println("Half age: " + (this.age / 0)); //This line introduces a new exception to deal with, since dividing by 0 is not possible and defined in Java this line throws a separate exception.
+        System.out.println("The person sprints...");       //But since exceptions are different classes and subclasses, and we defined to throw a regular exception when checking the age condition above
+    }                                                      //the method call in Main.java will also catch any exception, because we defined to do just that.
 }
