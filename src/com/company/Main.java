fdb18b916f3c4d68fb90042779016bb580676b50
MichaelFessler-Desktop <michaelfessler@gmx.at>
2022-04-13 10:33:01 +0200

    Forward unto Dawn!

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,6 +10,8 @@ public class Main {
             person1.sprint();
         } catch(TooOldException e) {
             System.out.println("The person is too old to sprint...");
+        } catch(ArithmeticException e) { //We can extend the try/catch block with another catch condition, in this case we catch the ArithmeticException (a RuntimeException) and handle it
+            System.out.println("Can't divide by 0 you donkey!"); //with this line. This will no longer lead to the program being terminated, and then execute the rest of the code instead.
         }
 
         System.out.println("test");
